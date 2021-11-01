7e7ce11a9ed5156a241efdcbc0cd12c7a73ff955
inbalLevi <71599740+inbalLevi@users.noreply.github.com>
2021-11-01 11:36:01 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,33 @@
 
 cd /home/Inbalevi0707/bootcamp-app 
 npm run dev    
-                 
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
                  
                  
                  
